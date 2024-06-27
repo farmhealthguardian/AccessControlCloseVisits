449c2313c5faa0d6410cd2996d39e75834fc958d
Jeff <wrathallj@gmail.com>
2024-06-27 16:51:36 -0400

    initial commit

---
+++ CloseVisits.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FHG.AccessControl
+{
+    public class CloseVisits
+    {
+        private readonly ILogger _logger;
+
+        public CloseVisits(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<CloseVisits>();
+        }
+
+        [Function("CloseVisits")]
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
