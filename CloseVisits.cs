b7791a14817b2d7e2de0093b234f7dbaa860ffaf
Jeff <wrathallj@gmail.com>
2024-07-31 17:34:13 -0400

    add EF and model

--- CloseVisits.cs
+++ CloseVisits.cs
@@ -1,22 +1,28 @@
 using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace FHG.AccessControl
 {
     public class CloseVisits
     {
         private readonly ILogger _logger;
+         private readonly ApplicationDbContext _context;
 
-        public CloseVisits(ILoggerFactory loggerFactory)
+        public CloseVisits(ILoggerFactory loggerFactory, ApplicationDbContext context)
         {
             _logger = loggerFactory.CreateLogger<CloseVisits>();
+            _context = context;
         }
 
         [Function("CloseVisits")]
         public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var accessControls = _context.Visit.Where(c=>c.OriginId == 33).ToList();
             
             if (myTimer.ScheduleStatus is not null)
             {
@@ -24,5 +30,35 @@ namespace FHG.AccessControl
             }
             //sample change
         }
+    }
+    public class Visit{
+  
+        public int Id { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public DateTime Date { get; set; }
+        public int OriginId { get; set; }
+        public int? DurationMinutes { get; set; }
+        public DateTime? ExitDate { get; set; }
+    }
+
+
+     public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(){}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Visit> Visit { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+            //TODO set certain strings as allowing null on creating
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using FHG.AccessControl;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+         services.AddDbContext<ApplicationDbContext>(options => 
+             options.UseSqlServer(Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection"))
+             .EnableDetailedErrors(true));
     })
     .Build();
-
 host.Run();
